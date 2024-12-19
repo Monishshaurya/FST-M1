42c0687260aa5f4145335ad87a6d3666abeddab0
Monishshaurya <kondamounika43@gmail.com>
2024-12-20 00:46:53 +0530

    Add files via upload

---
+++ Java/Activities/Activity2.java
@@ -0,0 +1,29 @@
+
+public class Activity2 {
+
+	public static void main(String[] args) 
+	{
+
+		int a[]= {10,77,10,54,-11,10};
+		int sum=0;
+		for(int i=0;i<a.length;i++) 
+		{
+			if (a[i]==10)
+				{
+				sum=sum+a[i];
+				}
+		}
+			if(sum==30) {
+				System.out.println("value is equal to 30");
+			}
+			else {
+				System.out.println("value is NOT equal to 30");
+
+			}
+		}
+		
+		
+		
+	
+
+}
