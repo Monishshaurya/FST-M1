cf19f63a93fefbe21460fcfdf8c25e8bc0557488
Monishshaurya <kondamounika43@gmail.com>
2025-01-01 18:13:00 +0530

    Add files via upload

---
+++ Java/Activities/Activity7.java
@@ -0,0 +1,13 @@
+
+public class Activity7
+{
+
+    public static void main(String args[])
+    {
+
+    	MountainBike mb=new MountainBike (3, 0, 23)	;
+    	System.out.println(mb.bicycleDesc());
+	    mb.applyBrake(20);
+	     mb.applyBrake(12);
+    }
+}
